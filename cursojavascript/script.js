693562c33e1a56bbc9536c5da2663013c35054b7
Bruna Angela <150439932+AngelaBruh@users.noreply.github.com>
2025-07-22 11:07:30 -0300

    Add files via upload

---
+++ cursojavascript/script.js
@@ -0,0 +1,95 @@
+
+
+/*let usdInput = document.querySelector("#usd")
+let brlInput = document.querySelector("#brl")
+
+usdInput.addEventListener("keyup", () => {
+    console.log(usdInput.value) // para saber o que foi escrito no campo
+})
+
+brlInput.addEventListener("keyup", () => {
+    console.log("apertou no campo brl")
+})
+*/
+
+
+
+
+
+/*
+function avisar(){
+    alert("Opa, disparei!")
+}
+
+//document.addEventListener("click", avisar)
+let botao = document.querySelector("#botao")
+
+// click - é um evento onde o usuario clica em algo
+// mouseover - é um evento onde o usuario passa o mouser em cima de algo
+botao.addEventListener("mouseover", () => {
+    avisar()
+}) 
+*/
+
+
+//alert("opa, aqui está um aviso")
+
+
+/*
+//tipos de variáveis:
+let idade = 18 // tipo number
+let nome = "Boniek" // tipo string
+let logado = false // tipo boolean
+
+let lista = ["farinha", "agua", "sal", "corante"] 
+
+for(let item of lista){
+    console.log("-Ingrediente: " + item)
+}
+
+
+
+
+
+if (idade >= 18){
+    console.log("Você é maior de idade")
+}
+
+
+if (nome == "Boniek"){
+    console.log("mensagem especial")
+}else{
+    console.log("mensagem normal")
+}
+
+
+
+
+
+// array e objetos
+let ingredientes = ["farinha", "agua", "sal", "corante"] 
+let personagem = {
+    nome: "fulano",
+    nivel: 10,
+    forca: 800,
+    magia : 200,
+    vida: 1000,
+    mana: 200
+}
+
+function somar(a, b){
+    let resultado = a + b;
+    return resultado
+}
+
+let somar2 = (a, b) => {
+    let resultado = a + b;
+    return resultado
+}
+
+let x = somar(15, 5)
+let y = somar2(2, 8)
+
+console.log(x)
+console.log(y)
+*/
